204365d487225b1fb51c3c3de0f7187656ab3a0a
ka-reem <71798166+ka-reem@users.noreply.github.com>
2024-12-13 19:13:33 -0800

    changes from yesterday

--- src/main/java/game/MinigameManager.java
+++ src/main/java/game/MinigameManager.java
@@ -1,4 +1,3 @@
-
 package game;
 
 import java.util.HashSet;
@@ -21,17 +20,32 @@ public class MinigameManager {
 
     public void markMinigameCompleted(Class<? extends Minigame> minigameClass) {
         completedMinigames.add(minigameClass);
+        System.out.println("[DEBUG] Minigame completed: " + minigameClass.getSimpleName());
+        System.out.println("[DEBUG] Current progress: " + toString());
     }
 
     public boolean isMinigameCompleted(Class<? extends Minigame> minigameClass) {
-        return completedMinigames.contains(minigameClass);
+        boolean completed = completedMinigames.contains(minigameClass);
+        System.out.println("[DEBUG] Checking completion for " + minigameClass.getSimpleName() + ": " + completed);
+        return completed;
     }
 
     public void resetProgress() {
+        System.out.println("[DEBUG] Resetting all minigame progress");
         completedMinigames.clear();
     }
 
     public int getCompletedCount() {
+        System.out.println("[DEBUG] Total completed minigames: " + completedMinigames.size());
         return completedMinigames.size();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("MinigameManager{completed=%d, games=%s}", 
+            completedMinigames.size(), 
+            completedMinigames.stream()
+                .map(Class::getSimpleName)
+                .toList());
     }
 }
